70fbca32feb82c5ed9f634eee1993d424d3fe98d
Kevin Sparling <kevinsparling@gmail.com>
2020-02-05 23:04:48 +0800

    gotta re-do some of this, it now should work in devel and prod

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 let bodyParser = require('body-parser');
-let mongoose = require('mongoose');
 
 const app = express();
 
@@ -14,13 +13,6 @@ app.use(bodyParser.json());
 
 // use the imported routes
 app.use('/', apiRoutes);
-
-
-var db = mongoose.connection;
-if(!db)
-    console.log("Error connecting db")
-else
-    console.log("Db connected successfully")
 
 
 const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
--- models/index.js
+++ models/index.js
@@ -1,7 +1,15 @@
 var mongoose = require('mongoose');
 mongoose.set('debug', true);
 
-const database_string = 'mongodb+srv://dbUser:futtbucker@cluster0-uthwo.mongodb.net/test?retryWrites=true&w=majority'
+
+let database_string = ''
+if (process.env.NODE_ENV === 'production') {
+    database_string = 'mongodb+srv://dbUser:futtbucker@cluster0-uthwo.mongodb.net/test?retryWrites=true&w=majority'
+    console.log(database_string)
+} else {
+    database_string = 'mongodb://127.0.0.1:27017/munenori_test'
+    console.log(database_string)
+}
 
 const db = mongoose.connect(database_string, { 
     useNewUrlParser: true, 
